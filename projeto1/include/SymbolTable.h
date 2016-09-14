c85fd0b098a350477e13d65a2aca81a66eec9cb2
Lucas May Petry <lucasmaypetry@gmail.com>
2016-09-14 15:37:28 -0300

    Tabela de símbolos criada.

---
+++ projeto1/include/SymbolTable.h
@@ -0,0 +1,62 @@
+#ifndef SYMBOLTABLE_H_
+#define SYMBOLTABLE_H_
+
+#include "TreeNode.h"
+#include <map>
+#include <string>
+
+class SymbolTable;
+
+extern SymbolTable SYMBOL_TABLE;
+extern void yyerror(const char* s, ...);
+
+/**
+ * Símbolo gerado na análise sintática.
+ */
+class Symbol {
+    
+    friend class SymbolTable;
+    
+    public:
+        enum DataType {
+            INTEGER
+        };
+    
+        enum IdentifierType {
+            VARIABLE
+        };
+    
+        Symbol();
+        Symbol(DataType dataType, IdentifierType idType, int64_t value, bool initialized);
+        virtual ~Symbol();
+    
+    private:
+        Symbol::DataType dataType;
+        Symbol::IdentifierType idType;
+        int64_t value;      /*Space to store a value while we are doing interpretation.*/
+        bool initialized;   /*Defines if symbol has been initialized or not.*/
+    
+};
+
+/**
+ * Tabela de símbolos gerada na análise sintática.
+ */
+class SymbolTable {
+    
+    public:    
+        SymbolTable();
+        virtual ~SymbolTable();
+    
+        /*checkId returns true if the variable has been defined and false if it does not exist*/
+        bool checkId(std::string id);
+        void addSymbol(std::string id, Symbol newSymbol);
+        TreeNode* newVariable(std::string id, TreeNode* next);
+        TreeNode* assignVariable(std::string id);
+        TreeNode* useVariable(std::string id);
+    
+    private:
+        std::map<std::string, Symbol> entryList;
+    
+};
+
+#endif
---
+++ projeto1/src/SymbolTable.cpp
@@ -0,0 +1,54 @@
+#include "SymbolTable.h"
+
+Symbol::Symbol() {
+    dataType = Symbol::INTEGER;
+    idType = Symbol::VARIABLE;
+    value = 0;
+    initialized = false;
+}
+
+Symbol::Symbol(DataType dataType, IdentifierType idType, int64_t value, bool initialized) : dataType(dataType), idType(idType), value(value), initialized(initialized) {
+}
+
+Symbol::~Symbol() {
+}
+
+SymbolTable::SymbolTable() {
+}
+
+SymbolTable::~SymbolTable() {
+}
+
+bool SymbolTable::checkId(std::string id) {
+    return entryList.find(id) != entryList.end();
+}
+
+void SymbolTable::addSymbol(std::string id, Symbol newSymbol) {
+    entryList[id] = newSymbol;
+}
+
+TreeNode* SymbolTable::newVariable(std::string id, TreeNode* next) {
+    if(checkId(id))
+        yyerror("Variable redefinition! %s\n", id.c_str());
+    else
+       addSymbol(id, Symbol(Symbol::INTEGER, Symbol::VARIABLE, 0, false)); // Adds variable to symbol table
+    
+    return 0;//new AST::Variable(id, next); //Creates variable node anyway
+}
+
+TreeNode* SymbolTable::assignVariable(std::string id) {
+    if(!checkId(id))
+        yyerror("Variable not defined yet! %s\n", id.c_str());
+    entryList[id].initialized = true;
+    
+    return 0;//new AST::Variable(id, NULL); //Creates variable node anyway
+}
+
+TreeNode* SymbolTable::useVariable(std::string id) {
+    if(!checkId(id))
+        yyerror("Variable not defined yet! %s\n", id.c_str());
+    if(!entryList[id].initialized)
+        yyerror("Variable not initialized yet! %s\n", id.c_str());
+    
+    return 0;//new AST::Variable(id, NULL); //Creates variable node anyway
+}
